2e092e7a6481acfe0ab095ecc37672bdb9238a84
Joel Wahlund <joel.wahlund@chas.se>
2014-11-25 22:19:40 +0100

    Added DTOS and EF reference

---
+++ SpelAffaren/SpelAffarWCF/SpelAffarenDTO.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpelAffarWCF
+{
+    public class GenreDto
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Namn")]
+        public string Namn { get; set; }
+        [Display(Name = "Produkter")]
+        public List<ProduktDto> Produkter { get; set; }
+    }
+
+    public class KonsolDto
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Namn")]
+        public string Namn { get; set; }
+        [Display(Name = "Produkter")]
+        public List<ProduktDto> Produkter { get; set; }
+    }
+
+    public class OrderDto
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Person som gjort order")]
+        public PersonerDto Person { get; set; }
+        [Display(Name = "Kommentar")]
+        public string Kommentar { get; set; }
+        [Display(Name = "Spel i ordern")]
+        public List<SpelPerOrderDto> SpelPerOrders { get; set; }
+    }
+
+    public class PersonerDto
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Förnamn")]
+        public string Förnamn { get; set; }
+        [Display(Name = "Efternamn")]
+        public string Efternamn { get; set; }
+        [Display(Name = "Email")]
+        public string LogOnEmail { get; set; }
+        [Display(Name = "Lösenord")]
+        public string Lösenord { get; set; }
+        [Display(Name = "Adminstatus")]
+        public bool AdminStatus { get; set; }
+        [Display(Name = "Ordrar")]
+        public List<OrderDto> Ordrar { get; set; }
+    }
+
+    public class ProduktDto
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Namn")]
+        public string Namn { get; set; }
+        [Display(Name = "Beskrivning")]
+        public string Beskrivning { get; set; }
+        [Display(Name = "Utgivningsår")]
+        public int Utgivningsår { get; set; }
+        [Display(Name = "Konsoler")]
+        public List<KonsolDto> Konsoler { get; set; }
+        [Display(Name = "Genres")]
+        public List<GenreDto> Genres { get; set; }
+        [Display(Name = "Spel i ordern")]
+        public List<SpelPerOrderDto> SpelPerOrders { get; set; }
+        [Display(Name = "Utgivare")]
+        public UtgivareDto Utgivare { get; set; }
+    }
+
+    public class SpelPerOrderDto
+    {
+        [Display(Name = "Antal av spelet")]
+        public int Antal { get; set; }
+        [Display(Name = "Spelet")]
+        public ProduktDto Produkt { get; set; }
+        [Display(Name = "Order")]
+        public OrderDto Order { get; set; }
+    }
+
+    public class UtgivareDto
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Namn")]
+        public string Namn { get; set; }
+        [Display(Name = "Produkter")]
+        public List<ProduktDto> Produkter { get; set; }
+    }
+}
